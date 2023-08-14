046195af2351b2415d462bc3ff6c092967bc3904
AnshulKumbhare <anshulkumbhare16@gmail.com>
2023-08-14 22:00:41 +0530

    Printing table of a Number

---
+++ TableOfNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TableOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.print("Enter d: ");
+        int d = sc.nextInt();
+
+        printTable(n,d);
+    }
+
+    private static void printTable(int n, int d){
+        int ans = 0;
+        for (int i=1; i<=d; i++){
+            ans = n*i;
+            System.out.printf("\n%d * %d = ",n,i);
+            System.out.print(ans+" ");
+        }
+    }
+}
