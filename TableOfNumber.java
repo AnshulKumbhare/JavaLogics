17a3b037ab61c99573eba510c340bb259f9c68c5
AnshulKumbhare <136897899+AnshulKumbhare@users.noreply.github.com>
2023-08-14 22:02:04 +0530

    Update TableOfNumber.java

--- TableOfNumber.java
+++ TableOfNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+/**The following code prints Table of a Number. */
 public class TableOfNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
